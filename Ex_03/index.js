130f36b553ddd6ee2d1a9260bf756980a38ded76
raoulbajj@mac <raoul.bajjani@epitech.eu>
2023-12-15 10:52:59 +0100

    UPDATE : final update

--- Ex_03/index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    // EXEMPLE DE L'UTILISATION DE PROMPT :
-
-    // Ici, on déclare une variable name qui va contenir la valeur saisie par l'utilisateur et qui pose la question "What is your name ?"
-    let name = prompt("What is your name ? :");
-
-    // Ici, on affiche dans la console le message "Hello" suivi de la valeur de la variable name
-    console.log("Hello " + name);
-};
